d1343d07a206bda20955cbea82056de29c1e7254
Cihan inan <cihanus@cihan-macbook.local>
2019-01-01 21:05:29 +0300

    Added comments as roadmap

---
+++ homework.py
@@ -0,0 +1,13 @@
+#generate random DNA sequence with length 200
+
+#find matching bases (ie 'ATG')
+
+#remove the noncoding bases before start codon
+
+#look for the stop codons
+
+#output the coding sequence
+
+#calculate GC content of the ORF
+
+#BLAST this sequence...
