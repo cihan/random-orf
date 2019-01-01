64816276c98c66bb7b9abaac270414e07fbb89a0
Cihan inan <cihanus@cihan.us>
2019-01-01 23:40:25 +0300

    Generates 200 base length DNA now

--- homework.py
+++ homework.py
@@ -1,5 +1,11 @@
 #generate random DNA sequence with length 200
-
+import random
+dna = ['a','t','g','c']
+query_list = []
+for i in range(201):
+	query_list.append(random.choice(dna))
+query_dna = ''.join(query_list)
+print(query_dna)
 #find matching bases (ie 'ATG')
 
 #remove the noncoding bases before start codon
